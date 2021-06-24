4fd91c1a45a61ef226019c031daa5fee04ddf85b
Alexanderh1988 <alex.utfsm@gmail.com>
2021-06-23 21:45:45 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+1) La fila debe estar completa con todos los datos (nombre, sexo, etc) para que se realice el archivo word, de lo contrario no se crea.
+2) Se recomienda guardar los datos de la tabla en un archivo texto (.txt) antes de cerrar el programa con el boton (guardar).
+4) Para retomar tabla incompleta se busca el archivo de texto (Datoscontr.txt) con el boton cargar.
+5) No borrar letras en negrillo del contrato base:
+"Nomb"(nombre),"RUT","Estadoc","Profesion","DIR"(direccion),"Nac"(nacionalidad) ,"SUE"(sueldo base),"Fechadeinicio" y "Fechadetermino".
+6) Luego de completar la tabla se busca el directorio del contrato base (ContratoBase.docx) a modificar con el boton "generar contratos" y los contratos generados se guardarán en el mismo directorio.
+7) Se tiene establecido un permiso al programa en la pagina de administrador: http://www.hstech.cl/Permisos/ con el codigo "LESBLRV". Debido a este paso es requerida conexion a internet para usar el programa.
